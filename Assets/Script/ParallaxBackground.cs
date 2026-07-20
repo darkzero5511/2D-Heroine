0b89c3ec126cf44b27ec1ec139f1b8ed0607fdff
darkzero5511 <darkzero551@gmail.com>
2026-07-20 18:53:03 +0700

    Update Background
    Endless Background

--- Assets/Script/ParallaxBackground.cs
+++ Assets/Script/ParallaxBackground.cs
@@ -3,24 +3,37 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
-    private float lastMainCameraPositionX;
+    private float lastCameraPositionX;
+    private float cameraHalfWidth;
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        InitializeLayers();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        float currentCameraPosstionX = mainCamera.transform.position.x;
-        float distanceToMove = currentCameraPosstionX - lastMainCameraPositionX;
-        lastMainCameraPositionX = currentCameraPosstionX;
+        float currentCameraPositionX = mainCamera.transform.position.x;
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionX;
+
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
 
         foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
         }
+    }
+
+    private void InitializeLayers()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+            layer.CalculateImageWidth();
     }
 }
